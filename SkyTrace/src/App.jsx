11d328e32131e14a0487155114f65add9eed4d15
Corbin Reak <corbinreak@gmail.com>
2026-07-29 10:35:16 -0600

    Successfully loaded leaflet map and centered over Northern Colorado

--- SkyTrace/src/App.jsx
+++ SkyTrace/src/App.jsx
@@ -1,11 +1,18 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 export default function App() {
+  const position = [40.3975, -105.0745]; // Base map for Loveland Co (My hometown)
+
   return (
-    <div className="navbar">
-      <nav className="navbar__container">
-        <div className="navbar_logo">
-          <h1>SkyTrace</h1>
-        </div>
-      </nav>
-    </div>
+    <MapContainer center={position} zoom={11} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>Loveland, CO</Popup>
+      </Marker>
+    </MapContainer>
   );
 }
